2d5f575d841f77cc601fd012a53d3c8947cc5b95
jstudenski <jstudenski@gmail.com>
2018-04-16 08:12:49 -0500

    fixed eslint errors in server.js

--- server.js
+++ server.js
@@ -1,44 +1,47 @@
 'use strict';
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const routes = require("./routes");
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-//initialize passport
+// initialize passport
 app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-//configure passport
+// configure passport
 const User = require('./models/user');
+
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
 // Serve up static assets
-app.use(express.static("client/build"));
+app.use(express.static('client/build'));
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/internshipAppDB");
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/internshipAppDB');
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
