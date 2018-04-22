e798a26b4d8686e49e61c32b849c429919508f79
jstudenski <jstudenski@gmail.com>
2018-04-22 18:57:05 -0500

    clean up app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Link } from "react-router-dom";
-
-import NoMatch from "./pages/NoMatch";
-// import Nav from "./components/Nav";
-import RegistrationForm from "./components/Users/RegistrationForm";
-import LoginForm from "./components/Users/LoginForm";
-import NewNeed from "./components/Needs/NewNeed";
-
 // Pages
 import Journal from "./pages/Journal";
 import Expenses from "./pages/Expenses";
 import UserLocation from "./pages/Location";
 import Requirements from "./pages/Requirements";
-// import Main from "./pages/Main";
-
+import NoMatch from "./pages/NoMatch";
+// Components
+import RegistrationForm from "./components/Users/RegistrationForm";
+import LoginForm from "./components/Users/LoginForm";
+import NewNeed from "./components/Needs/NewNeed";
+// Material UI components
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
-
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 
@@ -39,7 +35,6 @@ const App = () => (
           <Link to="/newNeed"><MenuItem>temp - New Need</MenuItem></Link>
         </Drawer>
         <Switch>
-          {/* Menu Items */}
           <Route exact path="/" component={Journal} />
           <Route exact path="/journal" component={Journal} />
           <Route exact path="/expenses" component={Expenses} />
