277eae2f34d3c7db6bd08573a17492191c1a8b27
jstudenski <jstudenski@gmail.com>
2018-04-21 23:25:24 -0500

    adding pages and navagation

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: () => axios.get("/api/books"),
+  // // Gets all books
+  // getBooks: () => axios.get("/api/books"),
   
-  // Gets the book with the given id
-  getBook: (id) => axios.get("/api/books/" + id),
+  // // Gets the book with the given id
+  // getBook: (id) => axios.get("/api/books/" + id),
 
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  },
+  // // Deletes the book with the given id
+  // deleteBook: function(id) {
+  //   return axios.delete("/api/books/" + id);
+  // },
+  // // Saves a book to the database
+  // saveBook: function(bookData) {
+  //   return axios.post("/api/books", bookData);
+  // },
 
   registerUser: data => axios.post("/api/users/register", data),
   loginUser: data => axios.post("/api/users/login", data),
