56215307267539f2d577eeafb31dab85f4981e2e
jstudenski <jstudenski@gmail.com>
2018-04-24 17:32:48 -0500

    removed left margin from individual pages and on to a container component

---
+++ client/src/components/ContentArea/ContentArea.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ContentArea = props => (
+  <div style={{marginLeft: 256 + 'px'}}>
+    {props.children}
+  </div>
+);
+
+export default ContentArea;
--- client/src/components/Users/RegistrationForm.js
+++ client/src/components/Users/RegistrationForm.js
@@ -56,7 +56,7 @@ export default class extends Component {
 
 	render() {
 		return (
-			<div style={{marginLeft: 256 + 'px'}}>
+			<div>
 				<h1>Example Registration Form</h1>
 				<h2>{this.state.currentUser && this.state.currentUser.email ? 
 					"User: " + this.state.currentUser.email : 
