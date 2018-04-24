544efefcb2798fbb5a0c1af4e1803a45aa8b3a95
jstudenski <jstudenski@gmail.com>
2018-04-24 17:37:17 -0500

    margin left fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // Pages
-import Journal from "./pages/Journal";
+import Journal from "./pages/Journal/";
 import Expenses from "./pages/Expenses/";
 import UserLocation from "./pages/Location";
 import Requirements from "./pages/Requirements";
@@ -11,6 +11,7 @@ import RegistrationForm from "./components/Users/RegistrationForm";
 import LoginForm from "./components/Users/LoginForm";
 import NewNeed from "./components/Needs/NewNeed";
 import Sidebar from "./components/Sidebar/Sidebar";
+import ContentArea from "./components/ContentArea/ContentArea";
 // Material UI components
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
@@ -22,17 +23,19 @@ const App = () => (
       <div>
         <AppBar title="International Intern" style={{ zIndex: '1600'}} />
         <Sidebar />
-        <Switch>
-          <Route exact path="/" component={Journal} />
-          <Route exact path="/journal" component={Journal} />
-          <Route exact path="/expenses" component={Expenses} />
-          <Route exact path="/location" component={UserLocation} />
-          <Route exact path="/requirements" component={Requirements} />
-          <Route exact path="/register" component={RegistrationForm} />
-          <Route exact path="/login" component={LoginForm} />
-          <Route exact path="/newNeed" component={NewNeed} />
-          <Route component={NoMatch} />
-        </Switch>
+        <ContentArea>
+          <Switch>
+            <Route exact path="/" component={Journal} />
+            <Route exact path="/journal" component={Journal} />
+            <Route exact path="/expenses" component={Expenses} />
+            <Route exact path="/location" component={UserLocation} />
+            <Route exact path="/requirements" component={Requirements} />
+            <Route exact path="/register" component={RegistrationForm} />
+            <Route exact path="/login" component={LoginForm} />
+            <Route exact path="/newNeed" component={NewNeed} />
+            <Route component={NoMatch} />
+          </Switch>
+        </ContentArea>
       </div>
     </Router>
   </MuiThemeProvider>
