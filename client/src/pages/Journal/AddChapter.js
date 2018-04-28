9fc4bc6a958449f5a5c64f6a51345e1a97b8ed40
Dani Carter <carte470@d.umn.edu>
2018-04-28 11:29:37 -0500

    chapter API 404 resolved

--- client/src/pages/Journal/AddChapter.js
+++ client/src/pages/Journal/AddChapter.js
@@ -11,19 +11,19 @@ const AddChapter = (props) => {
 			<h3> Add New Chapter</h3>
 			<form>
 				<p>Title</p>
-				<input name="chapterTitle" type="text" defaultValue={props.chapterTitle} onChange={this.handleInputChange} />
+				<input name="chapterTitle" type="text" value={props.chapterTitle} onChange={props.handleInputChange} />
 				<p>Description</p>
-				<input namme="description" tyoe="text" defaultValue={props.description} onChange={this.handleInputChange} />
+				<input namme="description" tyoe="text" defaultValue={props.description} onChange={props.handleInputChange} />
 				<p>Date</p>
-				<input name="date" type="date" defaultValue={props.date} placeholder={Date.now()} onChange={this.handleInputChange} />
+				<input name="date" type="date" value={props.date} placeholder={Date.now()} onChange={props.handleInputChange} />
 				<p>Image</p>
 		        <RaisedButton
 		          containerElement='label' label='My Label'>
-		          <input type="file" onChange={this.handleInputChange} />
+		          <input type="file" onChange={props.handleInputChange} />
 		        </RaisedButton>
 
 				<p>Requirement</p>
-				<DropDownMenu defaultValue={props.numReq} onChange={this.handleInputChange}>
+				<DropDownMenu value={props.numReq} onChange={props.handleInputChange}>
       				<MenuItem value={1} primaryText="1" />
       				<MenuItem value={2} primaryText="2" />
       				<MenuItem value={3} primaryText="3" />
@@ -31,7 +31,7 @@ const AddChapter = (props) => {
       				<MenuItem value={5} primaryText="5" />
     			</DropDownMenu>
 				<br />
-				<button className="btn btn-primary mt-2" onClick={this.submitForm}>Add Chapter</button>
+				<button className="btn btn-primary mt-2" onClick={props.handleFormSubmit}>Add Chapter</button>
 			</form>
 		</div>
 	)
