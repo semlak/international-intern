48f22452f0472cc25eb22300287a1b442eb717e4
jstudenski <jstudenski@gmail.com>
2018-04-27 20:39:20 -0500

    expense table created

--- client/src/pages/Expenses/ExpenseLedgerItem.js
+++ client/src/pages/Expenses/ExpenseLedgerItem.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import {
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
-const ExpenseLedgerItem = props => <tr><td style = {{paddingRight: 30}}>{props.expDate.slice(0, 10)}</td><td> </td><td>{props.expDesc}</td><td align="right">{props.expAmount.toFixed(2)}</td></tr>
+const ExpenseLedgerItem = props =>
+  <TableRow>
+    <TableRowColumn>{props.expDate.slice(0, 10)}</TableRowColumn>
+    <TableRowColumn>{props.expDesc}</TableRowColumn>
+    <TableRowColumn>{props.expAmount.toFixed(2)}</TableRowColumn>
+  </TableRow>;
+
+// <tr>
+//   <td style = {{paddingRight: 30}}>{props.expDate.slice(0, 10)}</td>
+//   <td> </td>
+//   <td>{props.expDesc}</td>
+//   <td align="right">{props.expAmount.toFixed(2)}</td>
+// </tr>
 
 export default ExpenseLedgerItem;
--- client/src/pages/Expenses/Ledger.js
+++ client/src/pages/Expenses/Ledger.js
@@ -1,21 +1,57 @@
-import React from "react";
+import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  // TableRowColumn,
+} from 'material-ui/Table';
+import Card from 'material-ui/Card';
 
-import ExpenseLedgerItem from "./ExpenseLedgerItem";
 
-const Ledger = (props) => (
-  <div className="expenseLedger" style={{marginLeft: 100 + 'px'}}>
-    <hr></hr>
-    <h1>Ledger</h1>
-    <table>
-      <thead>
-        <tr><th>Date</th><th> </th><th>Description</th><th>Amount USD</th><th>Amount XXX</th></tr>
-      </thead>
-      <tbody>
-        {props.expenses.map(expense => <ExpenseLedgerItem key={expense._id} {...expense}/>)}
-      </tbody>
-    </table>
-    <hr></hr>
-  </div>
+import ExpenseLedgerItem from './ExpenseLedgerItem';
+
+const Ledger = props => (
+// <h1>Ledger</h1>
+<Card>
+  <Table>
+    <TableHeader
+      displaySelectAll={false}
+      adjustForCheckboxes={false}
+    >
+      <TableRow>
+        <TableHeaderColumn>Date</TableHeaderColumn>
+        <TableHeaderColumn>Description</TableHeaderColumn>
+        <TableHeaderColumn>Amount USD</TableHeaderColumn>
+        <TableHeaderColumn>Amount XXX</TableHeaderColumn>
+      </TableRow>
+    </TableHeader>
+    <TableBody
+      //displaySelectAll={false}
+     // adjustForCheckboxes={false}
+    >
+      {props.expenses.map(expense => <ExpenseLedgerItem key={expense._id} {...expense} />)}
+    </TableBody>
+  </Table>
+  </Card>
 );
+
+// const Ledger = (props) => (
+//   <div className="expenseLedger" style={{marginLeft: 100 + 'px'}}>
+//     <hr></hr>
+//     <h1>Ledger</h1>
+//     <table>
+//       <thead>
+//         <tr><th>Date</th><th> </th><th>Description</th><th>Amount USD</th><th>Amount XXX</th></tr>
+//       </thead>
+//       <tbody>
+//         {props.expenses.map(expense => <ExpenseLedgerItem key={expense._id} {...expense}/>)}
+//       </tbody>
+//     </table>
+//     <hr></hr>
+//   </div>
+// );
+
 
 export default Ledger;
