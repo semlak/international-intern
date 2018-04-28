2fd0a781d9fd2abed544c3042c45f17bd375ae7e
Christopher Biessener <cpbiessener@gmail.com>
2018-04-27 20:17:26 -0500

    hard coded currency converion displays on Location page

--- client/src/pages/Location/Location.js
+++ client/src/pages/Location/Location.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Weather from "./Weather";
+import Weather from './Weather';
+import Currency from './Currency';
 
 const Location = () => (
   <div>
     <h1>Location</h1>
+    <hr />
     <Weather />
+    <hr />
+    <Currency />
   </div>
 );
 
--- client/src/pages/Location/Weather.js
+++ client/src/pages/Location/Weather.js
@@ -1,18 +1,19 @@
-import React, {Component} from 'react';
-import API from "../../utils/API";
+import React, { Component } from 'react';
+import API from '../../utils/API';
+import util from '../../utils/util';
 
 export default class extends Component {
   state = {
     currentUser: {},
     weather: {
-      'cityName': '',
-      'tempMinC': 0,
-      'tempMaxC': 0,
-      'tempMinF': 0,
-      'tempMaxF': 0,
-      'windSpeed': 0,
-      'windDirection': 0, 
-      'sky': '',
+      cityName: '',
+      tempMinC: 0,
+      tempMaxC: 0,
+      tempMinF: 0,
+      tempMaxF: 0,
+      windSpeed: 0,
+      windDirection: 0,
+      sky: '',
     },
   }
 
@@ -20,22 +21,21 @@ export default class extends Component {
 
   componentDidMount() {
     // fetch the user to get their location, etc...
-    API.getCurrentUser().then(response=> {
+    API.getCurrentUser().then((response) => {
       console.log('get user: ', response);
-      let currentUser = response.data.user
-      console.log('currentUser is: ' , currentUser);
-      this.setState({currentUser: currentUser});
+      const currentUser = response.data.user;
+      console.log('currentUser is: ', currentUser);
+      this.setState({ currentUser });
     });
 
     //
     // Now get the weather...
     //
     // Internship location
-    let city = 'Saint Paul'; // hard code for now
-    let country_code = 'US';
+    const city = 'Saint Paul'; // hard code for now
+    const country_code = 'US';
 
     const APIKey = process.env.REACT_APP_OPEN_WEATHERMAPS_API_KEY;
-  
     const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country_code}&appid=${APIKey}`;
     // api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + APIKey;
     // api.openweathermap.org/data/2.5/weather?q={city name}&appid=${APIKey}
@@ -48,32 +48,41 @@ export default class extends Component {
       const tempMaxC = (response.data.main.temp_max - 273.15).toFixed(1);
       const tempMinF = ((tempMinC * 1.8) + 32).toFixed(1);
       const tempMaxF = ((tempMaxC * 1.8) + 32).toFixed(1);
-  
-      this.setState({weather: {
-        'cityName': response.data.name,
-        'tempMinC': tempMinC,
-        'tempMaxC': tempMaxC,
-        'tempMinF': tempMinF,
-        'tempMaxF': tempMaxF,
-        'windSpeed': response.data.wind.speed,
-        'windDirection': response.data.wind.deg,
-        'sky': response.data.weather[0].main,
-      }});
+
+      this.setState({
+        weather: {
+          cityName: response.data.name,
+          tempMinC,
+          tempMaxC,
+          tempMinF,
+          tempMaxF,
+          windSpeed: response.data.wind.speed,
+          windDirection: response.data.wind.deg,
+          sky: response.data.weather[0].main,
+        },
+      });
       console.log('weather:', this.state.weather);
     }).catch((error) => {
       throw error;
-    });;
+    });
   }
 
   render() {
+    // html escape for 'degree celcius' : &#x2103;
+    const dC = { __html: '&#x2103;' };
+    // html escape for 'degree fahrenheight' : &#x2109;
+    const dF = { __html: '&#x2109;' };
+    // convert the wind direction (degrees) to a well-known ordinal
+    const ordinal = util.convertWind(this.state.weather.windDirection);
+
     return (
       <div>
         <h2>Weather</h2>
-        <p>Today's Low: {this.state.weather.tempMinF}</p>
-        <p>Today's High: {this.state.weather.tempMaxF}</p>
-        <p>Today's Wind: {this.state.weather.windSpeed} at {this.state.weather.windDirection} degrees</p>
-        <p>Today's Sky: {this.state.weather.sky}</p>
+        <p>Today{"'"}s Low: {this.state.weather.tempMinF}<span dangerouslySetInnerHTML={dF} /></p>
+        <p>Today{"'"}s High: {this.state.weather.tempMaxF}<span dangerouslySetInnerHTML={dF} /></p>
+        <p>Today{"'"}s Wind: {this.state.weather.windSpeed} {ordinal}</p>
+        <p>Today{"'"}s Sky: {this.state.weather.sky}</p>
       </div>
-    )
+    );
   }
 }
