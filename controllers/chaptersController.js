2d8b9be072260c1cf2dccea2bc3db9f5c91ed9b3
Joseph Semlak <semlak@gmail.com>
2018-04-29 19:55:18 -0500

    association chapter and user on cretaion

--- controllers/chaptersController.js
+++ controllers/chaptersController.js
@@ -3,7 +3,7 @@ const db = require('../models');
 // Defining methods for the chaptersController
 module.exports = {
   findAll: (req, res) => db.Chapter
-    .find()
+    .find({ user: req.user._id })
     .sort({ date: -1 })
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err)),
@@ -12,7 +12,7 @@ module.exports = {
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err)),
   create: (req, res) => db.Chapter
-    .create(req.body)
+    .create(Object.assign({}, req.body, { user: req.user._id }))
     .then(dbModel =>
       db.User.update({ _id: req.user._id }, { $push: { chapterRef: dbModel._id } })
         .then(() => res.json(dbModel))
