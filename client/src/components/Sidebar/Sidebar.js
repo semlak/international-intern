cce577f3c11cffba8b7e3177be8d55406b965ef6
jstudenski <jstudenski@gmail.com>
2018-04-30 19:56:45 -0500

    sidebar icons

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import Drawer from 'material-ui/Drawer';
 import { MenuList } from 'material-ui/Menu';
+
+// icons
+import Place from '@material-ui/icons/Place';
+import AttachMoney from '@material-ui/icons/AttachMoney';
+import AssignmentTurnedIn from '@material-ui/icons/AssignmentTurnedIn';
+import Create from '@material-ui/icons/Create';
+import Book from '@material-ui/icons/Book';
+
 import SidebarItem from './SidebarItem';
 
-// import { withStyles } from 'material-ui/styles';
 
 const links = [
   {
     text: 'Journal',
     path: '/journal',
+    icon: <Book />,
   }, {
     text: 'Expenses',
     path: '/expenses',
+    icon: <AttachMoney />,
   }, {
     text: 'Location',
     path: '/location',
+    icon: <Place />,
   }, {
     text: 'Requirements',
     path: '/requirements',
+    icon: <AssignmentTurnedIn />,
   }, {
     text: 'temp - Register',
     path: '/register',
+    icon: <Create />,
   },
 ];
 
@@ -45,6 +57,7 @@ const Sidebar = (props) => {
           key={link.text}
           text={link.text}
           path={link.path}
+          icon={link.icon}
         />
       ))}
       </MenuList>
--- client/src/pages/Journal/ChapterCard.js
+++ client/src/pages/Journal/ChapterCard.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ChapterContent from './ChapterContent';
 import './ChapterCard.css';
+import Card from 'material-ui/Card';
 
 const ChapterCard = (props) => (
+  <Card>
 	<div className="chapters">
 		<h1>Chapters</h1>
 		{props.chapters.map(chapter=>
@@ -14,6 +16,10 @@ const ChapterCard = (props) => (
 			</div>	
 		</div>)}
 	</div>
+
+  </Card>
+
+
 );
 
 export default ChapterCard;
