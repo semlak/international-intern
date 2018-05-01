7338e8bb46b1628a1912d6ef12600d4196279a2c
jstudenski <jstudenski@gmail.com>
2018-04-30 20:25:36 -0500

    began chapter cards

--- client/src/pages/Journal/ChapterCard.js
+++ client/src/pages/Journal/ChapterCard.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import ChapterContent from './ChapterContent';
 import './ChapterCard.css';
-import Card from 'material-ui/Card';
 
-const ChapterCard = (props) => (
-  <Card>
-	<div className="chapters">
-		<h1>Chapters</h1>
-		{props.chapters.map(chapter=>
-		<div className="card">
-			<div className="img-container">
-			</div>
-			<div className="chapterContent">
-				<ChapterContent key={chapter._id} {...chapter}/>	
-			</div>	
-		</div>)}
-	</div>
-
-  </Card>
-
-
+const ChapterCard = props => (
+  <div className="chapters">
+    <h1>Chapters</h1>
+    {props.chapters.map(chapter =>
+      <ChapterContent key={chapter._id} {...chapter} />)}
+  </div>
 );
 
 export default ChapterCard;
--- client/src/pages/Journal/ChapterContent.js
+++ client/src/pages/Journal/ChapterContent.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
-const ChapterContent = props =><div><p>{props.chapDate}</p><p>{props.chapTitle}</p>
-	<p>{props.chapNote}</p><p>{props.reqNum}</p></div>
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 
+
+const ChapterContent = props => (
+  <Card>
+    <CardMedia
+      style={{ height: 0, paddingTop: '56.25%' }}
+      image="https://upload.wikimedia.org/wikipedia/commons/2/21/Adams_The_Tetons_and_the_Snake_River.jpg"
+    />
+    <CardContent>
+      <p>{props.chapDate}</p>
+      <p>{props.chapTitle}</p>
+      <p>{props.chapNote}</p>
+      <p>{props.reqNum}</p>
+    </CardContent>
+  </Card>
+);
 export default ChapterContent;
