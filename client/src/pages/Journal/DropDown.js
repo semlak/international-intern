66b500cbeddd06a12a4e4570feaf26701d08a9f9
Dani Carter <carte470@d.umn.edu>
2018-05-04 17:00:13 -0500

    requirements showing in journal dropdown

---
+++ client/src/pages/Journal/DropDown.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const DropDownContent = props => (
+  <option value= {props.needNumber}>
+    {props.needNumber} {props.needTitle}
+  </option>
+
+);
+export default DropDownContent;
