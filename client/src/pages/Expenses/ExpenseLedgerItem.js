0d7a5a69a9c81e7a141f89396669d8b455974da6
Joseph <semlak@gmail.com>
2018-05-20 18:14:41 -0500

    removed $ symbole from expense table

--- client/src/pages/Expenses/ExpenseLedgerItem.js
+++ client/src/pages/Expenses/ExpenseLedgerItem.js
@@ -19,10 +19,8 @@ const ExpenseLedgerItem = props => (
     <CustomTableCell>{props.expDate.slice(0, 10)}</CustomTableCell>
     <CustomTableCell>{props.expDesc}</CustomTableCell>
     <CustomTableCell>{props.expCategory}</CustomTableCell>
-    <CustomTableCell numeric style={{ maxWidth: 5}}>
-      <span className="currencySymbol" style={{ float: 'left'}}>$</span>
-      <span className="usdCurrency" style={{ maxWidth: 2}}>{props.expAmount.toFixed(2)}</span>
-    </CustomTableCell>
+    <CustomTableCell numeric>{props.expAmount.toFixed(2)}</CustomTableCell>
+    {/* <CustomTableCell numeric>{props.expAmount.toFixed(2)}</CustomTableCell> */}
     <CustomTableCell numeric>{props.expAmountLocalCurrency.toFixed(2)}</CustomTableCell>
   </TableRow>
 
